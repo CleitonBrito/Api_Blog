743c1918c5426199a61ba0692ece7593eb121307
CleitonBrito <cleytonbritto3003@gmail.com>
2023-02-12 12:37:06 -0300

    remove sluggble

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use Sluggable;
     
     protected $fillable = [
         'title', 'content'
