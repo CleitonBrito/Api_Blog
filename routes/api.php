5c170bdccd571c57d6ea91a46a274cd5343a3f7f
CleitonBrito <cleytonbritto3003@gmail.com>
2023-02-17 15:43:58 -0300

    create routes api from comment votes

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\v1', 'middleware' => 'apiJwt
     Route::apiResource('/posts', 'PostController');
     Route::group(['prefix' => 'posts' ], function () {
         Route::apiResource('{post}/comments', 'CommentController');
+        Route::post('{post}/comments/{comment}/addOneVote', 'CommentController@addOneVote');
+        Route::post('{post}/comments/{comment}/subtractOneVote', 'CommentController@subtractOneVote');
     });
 });
