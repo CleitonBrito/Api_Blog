7fe84fb876e95467875dfdf6087a78bbfec6c9aa
CleitonBrito <cleytonbritto3003@gmail.com>
2023-02-17 15:40:12 -0300

    update fillable in model comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,7 +10,7 @@ class Comment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'author', 'comment', 'vote'
+        'post_id', 'author_id', 'comment', 'vote'
     ];
     
     public function post()
